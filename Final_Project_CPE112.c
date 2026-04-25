3426156e8b5ade9adb550d831ac7efc73ffc6338
Phuwanat K <acp528891@gmail.com>
2026-04-25 15:25:01 +0700

    Update Final_Project_CPE112.c

--- Final_Project_CPE112.c
+++ Final_Project_CPE112.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
 int deck();
@@ -12,19 +13,49 @@ int initail_column();       //linked-list
 int complete_stack();       //stack
 
 // ♠ ♥ ♦ ♣
+Card* createcard(int rank, char suit)
+{
+    Card *card = (Card *)malloc(sizeof(Card));
+    card->rank = rank;
+    card->suit = suit;
+    card->Faceup = 0;
+    card->next = NULL;
+    return card;
+}//Implemented by 3448; To create a card in the doubly linked list
 
-typedef struct card {
+typedef struct Card {
     int num;
+    bool Faceup;
     char suit;
-    struct card * next;
-} card;
+    struct Card * next;
+    struct Card * prev;
+} Card;
 
 int deck() {
+    Card *head = NULL;
+    Card *tail = NULL;
     char suit[4] = {"♠", "♥", "♦", "♣"};
-    for(int i = 0; i < 13; i++) {
-        
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 1; j <= 13; j++) 
+            {
+                Card *newCard = createcard(j, suit[i]);
+
+                if (head == NULL)
+                {
+                    head = newCard;
+                    tail = newCard;
+                    newCard->prev = NULL;
+                }
+                else
+                {
+                    newCard->prev = tail;
+                    tail->next = newCard;
+                    tail = newCard;
+                }
+            }
     }
-}
+} //Implemented by 3448; initalizing the deck by using doubly linked list starting from each suits, and from ace to king 
 
 int main() {
 
